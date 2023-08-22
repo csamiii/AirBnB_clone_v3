9001e720b9d1cf654718e669503020ad0339c8d0
csamiii <chidisamuel16@gmail.com>
2023-08-22 15:03:36 +0000

    codebase

--- models/base_model.py
+++ models/base_model.py
@@ -10,6 +10,7 @@ import sqlalchemy
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 import uuid
+import hashlib
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
 
@@ -63,11 +64,17 @@ class BaseModel:
         new_dict = self.__dict__.copy()
         if "created_at" in new_dict:
             new_dict["created_at"] = new_dict["created_at"].strftime(time)
+
         if "updated_at" in new_dict:
             new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
         new_dict["__class__"] = self.__class__.__name__
+
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
+
+        if getenv("HBNB_TYPE_STORAGE") == "db":
+            if 'password' in new_dict:
+                del new_dict["password"]
         return new_dict
 
     def delete(self):
