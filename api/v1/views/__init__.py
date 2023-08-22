e8886e41d7bca9a821f192b89f351404871cedfd
csamiii <chidisamuel16@gmail.com>
2023-08-22 16:00:17 +0000

    update

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """Init file"""
 from flask import Blueprint
 
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -13,8 +13,8 @@ def status():
 def stats():
     """Returns the number of each instance type."""
     return jsonify(amenities=storage.count("Amenity"),
-                   cities=storage.count("City"),
-                   places=storage.count("Place"),
-                   reviews=storage.count("Review"),
-                   states=storage.count("State"),
-                   users=storage.count("User"))
+                    cities=storage.count("City"),
+                    places=storage.count("Place"),
+                    reviews=storage.count("Review"),
+                    states=storage.count("State"),
+                    users=storage.count("User"))
